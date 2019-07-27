d9ef6511c2b694bc6bc192d1861dadf557a67fa6
Michal Walczynski <mwalczynski@pgs-soft.com>
2019-07-27 12:47:17 +0200

    Add project files.

---
+++ BenchmarkExample.PerformanceTests/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace BenchmarkExample.PerformanceTests
+{
+    using BenchmarkDotNet.Running;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<ResponseBenchmarks>();
+        }
+    }
+}
---
+++ BenchmarkExample.PerformanceTests/ResponseBenchmarks.cs
@@ -0,0 +1,43 @@
+﻿namespace BenchmarkExample.PerformanceTests
+{
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using BenchmarkDotNet.Attributes;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Mvc.Testing;
+    using Microsoft.Extensions.Logging;
+
+    [InProcess]
+    [MemoryDiagnoser]
+    public class ResponseBenchmarks
+    {
+        private HttpClient _client;
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            var factory = new WebApplicationFactory<Startup>()
+                .WithWebHostBuilder(configuration =>
+                {
+                    configuration.ConfigureLogging(logging =>
+                    {
+                        logging.ClearProviders();
+                    });
+                });
+
+            _client = factory.CreateClient();
+        }
+
+        [Benchmark]
+        public Task GetAllValuesResponseTime()
+        {
+            return _client.GetAsync("/api/values");
+        }
+
+        [Benchmark]
+        public Task GetValueResponseTime()
+        {
+            return _client.GetAsync("/api/values/5");
+        }
+    }
+}
